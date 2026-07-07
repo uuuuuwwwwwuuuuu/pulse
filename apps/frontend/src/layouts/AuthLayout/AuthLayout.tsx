c5bb79a97b1e3b917ccb4feaab95fa688d50ba27
Denis <devorlich@gmail.com>
2026-07-07 20:36:31 +0300

    session check with toast notification in Main and Auth Layouts for unauthorized access.

--- apps/frontend/src/layouts/AuthLayout/AuthLayout.tsx
+++ apps/frontend/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,24 +1,18 @@
 import { type FC, useEffect, Suspense } from 'react';
-import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useSession } from '@api/auth';
 import { PageLoader } from '@components/PageLoader/PageLoader';
 import styles from './AuthLayout.module.scss';
 
 export const AuthLayout: FC = () => {
     const { data: session, isPending } = useSession();
-
-    const location = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!isPending && session) {
-            if (location.pathname === '/auth/sign-in') {
-                navigate('/', { replace: true });
-            } else if (location.pathname === '/auth/sign-up') {
-                navigate('/organization', { replace: true });
-            }
+            navigate('/organization', { replace: true });
         }
-    }, [session, isPending, navigate, location.pathname]);
+    }, [session, isPending, navigate]);
 
     if (isPending) {
         return null;
--- apps/frontend/src/layouts/MainLayout/MainLayout.tsx
+++ apps/frontend/src/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,27 @@
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import styles from './MainLayout.module.scss';
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSession } from "@api/auth";
+import { toast } from 'react-hot-toast';
 
 export const MainLayout: FC = () => {
+    const { data: session, isPending } = useSession();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        if (isPending) {
+            return;
+        }
+
+        if (!session) {
+            navigate('/auth/sign-in');
+            toast('You are not authorized to access this page', {
+                icon: '❌',
+                duration: 3000,
+            });
+        }
+    }, [session, isPending, navigate]);
+
     return (
         <div className={styles.mainLayout}>
             <Outlet />
